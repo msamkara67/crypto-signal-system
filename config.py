cae2be43ec23a103f9d0708aedfdcda3d62d1147
msamkara67 <msamkara@gmail.com>
2025-08-02 16:23:20 +0300

    Create config.py
    Add config.py including file names on the path

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+working_dir_name = os.path.join(os.path.expanduser("~"), "Desktop", "coin_Colosseum")
+
+template_path = os.path.join(working_dir_name, "coin_data_template.xlsx")
+data_path = os.path.join(working_dir_name, "coin_data_180days_top100.xlsx")
